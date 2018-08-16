ee28d42a9e4eaa9bc279f8183d401876fd450048
Josh Berkus <jberkus@redhat.com>
2018-08-15 18:22:50 -0700

    Some additional fixes

--- fix_selinux_perms.sh
+++ fix_selinux_perms.sh
@@ -4,3 +4,7 @@ for i in {/var/lib/etcd,/etc/kubernetes/pki,/etc/kubernetes/pki/etcd,/etc/cni/ne
 do
   mkdir -p $i && chcon -Rt svirt_sandbox_file_t $i
 done
+
+# fix iptables, just in case
+
+echo 1 >/proc/sys/net/bridge/bridge-nf-call-iptables
